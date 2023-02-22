861fc45d22e1841c18c5534170eea7c8be19a12d
Ray Yuan Liu <louis383@gmail.com>
2023-02-22 11:11:41 +0800

    Add two plugins: harpoon and undotree

--- lua/ray/init.lua
+++ lua/ray/init.lua
@@ -1,2 +1,14 @@
 require("ray.remap")
 print("hello from the ray directory")
+
+-- 自訂縮排 (Tab) 位元數
+local set = vim.opt -- set options
+-- width of tab character
+set.tabstop = 4
+-- fine tunes the amount of whitespace to be added
+set.softtabstop = 4
+-- determines the amount of whitespace to add in normal mode
+set.shiftwidth = 4
+
+set.expandtab = true
+set.smartindent = true
